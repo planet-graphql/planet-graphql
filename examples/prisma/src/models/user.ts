73bef07326d63c03e3a710c907b9e480d1e7b9b8
dekimasoon <dekimasoon@gmail.com>
2022-07-28 07:08:08 +0000

    chore: move user field resolvers to user-resolver file

--- examples/prisma/src/models/user.ts
+++ examples/prisma/src/models/user.ts
@@ -1,37 +1,14 @@
-import { prisma, getRelations } from '../'
-import { inputs, pg, pgpc } from '../graphql'
-import { post, postWithoutRelation } from './post'
+import { getRelations } from '../'
+import { inputs, pgpc } from '../graphql'
+import { postWithoutRelation } from './post'
 
-export const user = pgpc
-  .update({
-    name: 'User',
-    fields: (f, b) => ({
-      ...f,
-      posts: b
-        .relation(() => post)
-        .list()
-        .prismaArgs(() => inputs.findManyPost.build()),
-      fullName: b.string(),
-      latestPost: b.object(() => postWithoutRelation).nullable(),
-    }),
-    relations: () => getRelations('User'),
-  })
-  .modify((f) => ({
-    fullName: f.fullName.resolve(({ source }) => source.firstName + source.lastName),
-    latestPost: f.latestPost.resolve((params) => {
-      return pg.dataloader(params, async (list) => {
-        const posts: any[] = await prisma.$queryRaw`
-          select *
-          from Post
-          where
-            authorId in (${list.map((x) => x.id).join(',')}) and
-            not exists (
-              select *
-              from Post as InnerPost
-              where InnerPost.authorId = Post.authorId AND InnerPost.createdAt > Post.createdAt
-            )
-        `
-        return list.map((user) => posts.find((x) => x.authorId === user.id) ?? null)
-      })
-    }),
-  }))
+export const user = pgpc.update({
+  name: 'User',
+  fields: (f, b) => ({
+    ...f,
+    posts: f.posts.prismaArgs(() => inputs.findManyPost.build()),
+    fullName: b.string(),
+    latestPost: b.object(() => postWithoutRelation).nullable(),
+  }),
+  relations: () => getRelations('User'),
+})
--- examples/prisma/src/resolvers/user-resolvers.ts
+++ examples/prisma/src/resolvers/user-resolvers.ts
@@ -1,12 +1,34 @@
 import { prisma } from '..'
 import { inputs, pg } from '../graphql'
 import { user } from '../models/user'
+import { Prisma } from '../prisma-client'
+
+user.modify((f) => ({
+  fullName: f.fullName.resolve(({ source }) => source.firstName + source.lastName),
+  latestPost: f.latestPost.resolve((params) => {
+    return pg.dataloader(params, async (list) => {
+      const posts: any[] = await prisma.$queryRaw`
+        SELECT *
+        FROM "Post"
+        WHERE
+          "authorId" IN (${Prisma.join(list.map((x) => x.id))}) AND
+          NOT EXISTS (
+            SELECT *
+            FROM "Post" AS InnerPost
+            WHERE InnerPost."authorId" = "Post"."authorId" AND InnerPost."createdAt" > "Post"."createdAt"
+          )
+      `
+      return list.map((user) => posts.find((x) => x.authorId === user.id) ?? null)
+    })
+  }),
+}))
 
 export const usersQuery = pg.query({
   name: 'users',
   field: (b) =>
     b
       .object(() => user)
+      .auth(({ context }) => context.isAdmin)
       .relay()
       .prismaArgs(() =>
         inputs.findManyUser
